12b7f57db7c244f8346505c08357796bea9fa7df
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-01-19 17:35:19 +0900

    메뉴손봄

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const menu = document.querySelectorAll('.text');
+const listMenu = document.querySelectorAll('.menu');
+
+console.log(listMenu);
 
 
 
@@ -12,7 +15,22 @@ function handleMouseOut(event){
     subMenu.classList.add('displayNone');
 }
 
+function handleMouseInList(event){
+    const listMenu=event.target;
+    listMenu.classList.remove('displayNone');
+}
+
+function handleMouseOutList(event){
+    const listMenu=event.target;
+    listMenu.classList.add('displayNone');
+}
+
 menu.forEach(element => {
     element.addEventListener("mouseenter", handleMouseIn);
     element.addEventListener("mouseleave", handleMouseOut);
-});
+})
+
+listMenu.forEach(element =>{
+    element.addEventListener("mouseenter", handleMouseInList);
+    element.addEventListener("mouseenter", handleMouseOutList);
+})
