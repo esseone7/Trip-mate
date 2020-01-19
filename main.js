305b1655a236c2c3cf85a327d01f4bbe5c597045
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-01-19 17:49:57 +0900

    메뉴수정
    메뉴수정

--- main.js
+++ main.js
@@ -1,36 +1,24 @@
 const menu = document.querySelectorAll('.text');
-const listMenu = document.querySelectorAll('.menu');
+const listMenu = document.querySelectorAll('.listMenu');
 
-console.log(listMenu);
+
 
 
 
 function handleMouseIn(event){
-    const subMenu=event.target.nextElementSibling;
+    const subMenu=event.target.children[1];
     subMenu.classList.remove('displayNone');
 }
 
 function handleMouseOut(event){
-    const subMenu=event.target.nextElementSibling;
+    const subMenu=event.target.children[1];
     subMenu.classList.add('displayNone');
 }
 
-function handleMouseInList(event){
-    const listMenu=event.target;
-    listMenu.classList.remove('displayNone');
-}
 
-function handleMouseOutList(event){
-    const listMenu=event.target;
-    listMenu.classList.add('displayNone');
-}
 
-menu.forEach(element => {
-    element.addEventListener("mouseenter", handleMouseIn);
-    element.addEventListener("mouseleave", handleMouseOut);
-})
 
 listMenu.forEach(element =>{
-    element.addEventListener("mouseenter", handleMouseInList);
-    element.addEventListener("mouseenter", handleMouseOutList);
+    element.addEventListener("mouseenter", handleMouseIn);
+    element.addEventListener("mouseleave", handleMouseOut);
 })
