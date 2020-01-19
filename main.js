2b51dc58a23a0741a170740f42a2ecc4044e7942
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-01-19 18:15:22 +0900

    위젯(중앙 메세지)
    중앙에 메세지 추가

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const menu = document.querySelectorAll('.text');
 const listMenu = document.querySelectorAll('.listMenu');
+const message = document.querySelector('.message');
 
 
 
@@ -15,10 +15,22 @@ function handleMouseOut(event){
     subMenu.classList.add('displayNone');
 }
 
-
+function displayMessage(){
+    let ranNum = parseInt(Math.random()*1000);
+    let innerMessage='';
+    if (ranNum<10){
+        innerMessage=`00${ranNum}`;
+    } else if(ranNum<100){
+        innerMessage=`0${ranNum}`;
+    } else {
+        innerMessage=`${ranNum}`;
+    }
+    message.innerHTML=`지금 주변에 ${innerMessage} 명의 회원들이 여행 메이트를 찾고 있습니다!!`;
+}
 
 
 listMenu.forEach(element =>{
     element.addEventListener("mouseenter", handleMouseIn);
     element.addEventListener("mouseleave", handleMouseOut);
 })
+setInterval(displayMessage, 1000);
